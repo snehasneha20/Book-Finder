38f3c01c6084f9363509fbbdae02a1334cc329af
snehasneha20 <147418644+snehasneha20@users.noreply.github.com>
2025-10-27 21:48:04 +0530

    Create react.js

---
+++ react.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// Create React root and render the App
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
